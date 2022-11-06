85e183eef3462001dc2cf88ed693793fdcc2c8b8
radio0but <radio0but@gmail.com>
2022-11-06 18:15:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,20 +11,20 @@ else
 fi
 
 # Correct permission ~/.config
-sudo mkdir -p ~/.config
-sudo chown mastodon:mastodon ~/.config
+ mkdir -p ~/.config
+ chown mastodon:mastodon ~/.config
 
 # Clone Mastodon
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y git curl ufw
+ apt update
+ apt upgrade -y
+ apt install -y git curl ufw
 git clone https://github.com/mastodon/mastodon.git ~/live
 cd ~/live
 
 # Install packages
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install -y npm 
-sudo apt install -y \
+curl -sL https://deb.nodesource.com/setup_12.x |  -E bash -
+ apt install -y npm 
+ apt install -y \
   ufw imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core \
   g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc autoconf \
   bison build-essential libssl-dev libyaml-dev libreadline6-dev \
@@ -54,28 +54,28 @@ echo N | rbenv install $(cat ~/live/.ruby-version)
 rbenv global $(cat ~/live/.ruby-version)
 
 # Setup ufw
-printf y | sudo ufw enable
-sudo ufw allow 80
-sudo ufw allow 443
-sudo ufw allow 22 #sshシャットアウト対策
+printf y |  ufw enable
+ ufw allow 80
+ ufw allow 443
+ ufw allow 22 #sshシャットアウト対策
 
 # Install yarn
-sudo npm install -g yarn 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+ npm install -g yarn 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg |  apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" |  tee /etc/apt/sources.list.d/yarn.list
 
 # Obtain SSL Cert
 if [ "$getCERT_FLAG" == "y" -o "$getCERT_FLAG" == "Y" ]
 then 
-  sudo apt install -y certbot python3-certbot-nginx 
-  sudo certbot certonly -d $INSTANCE -m $EMAIL -n --nginx --agree-tos
-  echo "@daily certbot renew --renew-hook \"service nginx restart\"" | sudo tee -a /etc/cron.d/certbot-renew 
+   apt install -y certbot python3-certbot-nginx 
+   certbot certonly -d $INSTANCE -m $EMAIL -n --nginx --agree-tos
+  echo "@daily certbot renew --renew-hook \"service nginx restart\"" |  tee -a /etc/cron.d/certbot-renew 
 else 
   echo ""
 fi 
 # Setup PostgreSQL
 set +e
-echo "CREATE USER mastodon CREATEDB" | sudo -u postgres psql -f -
+echo "CREATE USER mastodon CREATEDB" |  -u postgres psql -f -
 set -e 
 
 # Setup Mastodon 
@@ -98,11 +98,13 @@ then
 else
   echo "" > /dev/null
 fi
-sudo ln -s /home/mastodon/live/dist/$SERVER_DOMAIN.conf /etc/nginx/conf.d/$SERVER_DOMAIN.conf
+ ln -s /home/mastodon/live/dist/$SERVER_DOMAIN.conf /etc/nginx/conf.d/$SERVER_DOMAIN.conf
 
 # Set up systemd services
-sudo cp /home/mastodon/live/dist/mastodon-*.service /etc/systemd/system/
-sudo systemctl enable --now mastodon-web.service mastodon-streaming.service mastodon-sidekiq.service
-sudo systemctl restart nginx.service
+ cp /home/mastodon/live/dist/mastodon-*.service /etc/systemd/system/
+ systemctl enable --now mastodon-web.service mastodon-streaming.service mastodon-sidekiq.service
+ systemctl restart nginx.service
+
+
 
 
